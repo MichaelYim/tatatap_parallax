54bc42d9fb78f95016c26a6f033d0a7248c0871b
Tai Yin Michae Yim <mikeyimmy@gmail.com>
2014-09-18 16:36:06 +0800

    bg image moves 50%

--- js/aptitude.js
+++ js/aptitude.js
@@ -1,1 +1,25 @@
-// document.documentElement.clientHeight
+$(document).ready(function(){
+  // Background moves 50% of scroll
+  var $window = $(window);
+
+  $('section[data-type="background"]').each(function(){
+    var $bg = $(this); // assign the object
+      $(window).scroll(function() {
+        var yPos = -($window.scrollTop() / $bg.data('speed'));
+        // final background position
+        var coords = '50% '+ yPos + 'px';
+        // Move background
+        $bg.css({ backgroundPosition: coords });
+      });
+  });
+
+  // Opacity change when bottom bar reaches top bar
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      $(".top-item").fadeTo(1000, 0.8);
+
+    }
+  });
+
+
+});
